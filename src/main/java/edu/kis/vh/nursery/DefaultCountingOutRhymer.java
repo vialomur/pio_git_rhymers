b18a59b9fd4c0af1ee359d2b6624773c40c8eb69
vialomur <maxim44m@ukr.net>
2023-04-25 18:47:16 +0200

    Dodane gettery i settery razem z hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,12 +2,16 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int MAX_SIZE = 12;
-    public static final int EMPTY_RHYMER_INDICATOR = -1;
-    public static final int DEFAULT_VALUE = -1;
+    private static final int MAX_SIZE = 12;
+    private static final int EMPTY_RHYMER_INDICATOR = -1;
+    private static final int DEFAULT_VALUE = -1;
     private final int[] numbers = new int[MAX_SIZE];
 
-    public int total = EMPTY_RHYMER_INDICATOR;
+    public int getTotal() {
+        return total;
+    }
+
+    private int total = EMPTY_RHYMER_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
