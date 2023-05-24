25441114ec76b500a94a1e51a782c83ca99ec71f
vialomur <maxim44m@ukr.net>
2023-05-24 10:33:27 +0200

    poprawilem bledy zeby znow dzialalo

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,17 +3,17 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-    
+
     @Override
     public int countOut() {
         while (!callCheck())
             temp.countIn(super.countOut());
-        
+
         int ret = temp.countOut();
-        
+
         while (!temp.callCheck())
             countIn(temp.countOut());
-        
+
         return ret;
     }
 }
