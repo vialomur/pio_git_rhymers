37dd8a71dbad67a01b0dcfe13dbb56f58deb4ebb
vialomur <maxim44m@ukr.net>
2023-05-24 10:53:59 +0200

    uporzadkowalem rhymersdemo rozbijajac main na mniejsze metody

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -11,23 +11,33 @@ class RhymersDemo {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
+        fillRhymers(rhymers);
+        generateRandomNumbers(rhymers[3]);
+        printRhymers(rhymers);
+        printRejectedCount(rhymers[3]);
+
+    }
+    private static void fillRhymers(DefaultCountingOutRhymer[] rhymers) {
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
+    }
 
+    private static void generateRandomNumbers(DefaultCountingOutRhymer rhymer) {
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+            rhymer.countIn(rn.nextInt(20));
+    }
 
+    private static void printRhymers(DefaultCountingOutRhymer[] rhymers) {
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-
-        System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
-
     }
 
+    private static void printRejectedCount(DefaultCountingOutRhymer rhymer) {
+        System.out.println("total rejected is " + ((HanoiRhymer) rhymer).reportRejected());
+    }
 }
