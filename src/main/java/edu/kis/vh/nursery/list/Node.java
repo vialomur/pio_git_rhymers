9b5ea5c6c53e476df0db9b44acdd29f412c7d191
vialomur <maxim44m@ukr.net>
2023-05-24 11:05:01 +0200

    created java doc for Node class

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,12 +1,35 @@
 package edu.kis.vh.nursery.list;
 
+
+/**
+
+ The Node class represents a node in a linked list.
+
+ Each node stores an integer value and maintains references to the previous and next nodes.
+ */
 public class Node {
 
+    /**
+
+     The value stored in the node.
+     */
     public final int value;
-    public Node prev, next;
+    /**
 
-    public Node(int i) {
-        value = i;
+     Reference to the previous node in the linked list.
+     */
+    public Node prev;
+    /**
+
+     Reference to the next node in the linked list.
+     */
+    public Node next;
+    /**
+
+     Constructs a new Node with the specified integer value.
+     @param value the integer value to be stored in the node
+     */
+    public Node(int value) {
+        this.value = value;
     }
-
 }
